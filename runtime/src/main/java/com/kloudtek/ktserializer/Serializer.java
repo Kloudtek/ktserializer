dbb27d4e5b29576f7d31254474ef0d0c2f22cee5
Yannick Menager <ymenager@kloudtek.com>
2016-01-26 15:06:26 -0800

    fixing bugs

--- runtime/src/main/java/com/kloudtek/ktserializer/Serializer.java
+++ runtime/src/main/java/com/kloudtek/ktserializer/Serializer.java
@@ -6,18 +6,18 @@ package com.kloudtek.ktserializer;
 
 import com.kloudtek.util.SystemUtils;
 import com.kloudtek.util.UnexpectedException;
-import com.kloudtek.util.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.List;
+import java.net.URLDecoder;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.logging.Level;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.logging.Logger;
 
 /**
@@ -25,6 +25,7 @@ import java.util.logging.Logger;
  */
 public class Serializer {
     private static final Logger logger = Logger.getLogger(Serializer.class.getName());
+    public static final String META_INF_KTSERIALIZER = "META-INF/ktserializer";
     private static ClassMapper systemClassMapper = new ClassMapper();
     private static final SerializationEngine globalInstance = new SerializationEngine(systemClassMapper);
 
@@ -51,43 +52,57 @@ public class Serializer {
 
     private static void loadConfig() {
         try {
-            Enumeration<URL> resources = ClassMapper.class.getClassLoader().getResources("META-INF/ktserializer");
+            Enumeration<URL> resources = ClassMapper.class.getClassLoader().getResources(META_INF_KTSERIALIZER);
             while (resources.hasMoreElements()) {
                 URL url = resources.nextElement();
-                logger.info("Loading config file: " + url);
-                InputStream is = url.openStream();
-                try {
-                    if (is != null) {
-                        try {
-                            logger.info("IS=" + is);
-                            String filesStr = IOUtils.toString(is).trim();
-                            String[] files = filesStr.split("\n");
-                            for (String file : files) {
-                                loadConfig(file.trim());
-                            }
-                        } finally {
-                            IOUtils.close(is);
-                        }
-                    } else {
-                        logger.info("Unable to load " + url + " stream is null");
-                    }
-                } catch (Throwable e) {
-                    logger.log(Level.WARNING, e.getMessage(), e);
+                for (String file : listFiles(url)) {
+                    loadConfig(file.trim());
                 }
             }
         } catch (IOException e) {
             throw new InvalidConfigException(e);
         } catch (NumberFormatException e) {
             throw new InvalidConfigException(e);
-//        } catch (ClassNotFoundException e) {
-//            throw new InvalidConfigException(e);
+        } catch (ClassNotFoundException e) {
+            throw new InvalidConfigException(e);
         } catch (ClassCastException e) {
             throw new InvalidConfigException(e);
-//        } catch (InstantiationException e) {
-//            throw new InvalidConfigException(e);
-//        } catch (IllegalAccessException e) {
-//            throw new InvalidConfigException(e);
+        } catch (InstantiationException e) {
+            throw new InvalidConfigException(e);
+        } catch (IllegalAccessException e) {
+            throw new InvalidConfigException(e);
         }
+    }
+
+    private static String[] listFiles(@NotNull URL url) throws IOException {
+        if (url.getProtocol().equals("file")) {
+            try {
+                return new File(url.toURI()).list();
+            } catch (URISyntaxException e) {
+                throw new UnexpectedException(e);
+            }
+        }
+
+        if (url.getProtocol().equals("jar")) {
+            String jarPath = url.getPath().substring(5, url.getPath().indexOf("!"));
+            JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
+            Enumeration<JarEntry> entries = jar.entries();
+            Set<String> result = new HashSet<String>();
+            while (entries.hasMoreElements()) {
+                String name = entries.nextElement().getName();
+                if (name.startsWith(META_INF_KTSERIALIZER)) {
+                    String entry = name.substring(META_INF_KTSERIALIZER.length());
+                    int checkSubdir = entry.indexOf("/");
+                    if (checkSubdir >= 0) {
+                        entry = entry.substring(0, checkSubdir);
+                    }
+                    result.add(entry);
+                }
+            }
+            return result.toArray(new String[result.size()]);
+        }
+
+        throw new UnsupportedOperationException("Cannot list files for URL " + url);
     }
 
     private static void loadConfig(String path) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException {
